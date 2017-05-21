52b571741b57b69c9195fa8bc5fabb044ce325f9
Andrey <a5kin@users.noreply.github.com>
2017-05-21 03:20:59 +0300

    Fullscreen temporary solution.

--- moire/main.py
+++ moire/main.py
@@ -1,10 +1,16 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.config import Config
+
+
+Config.set('graphics', 'fullscreen', 'auto')
+Config.write()
 
 
 class MainEngine(Widget):
-    
+
     def update(self, dt):
         pass
 
@@ -15,8 +21,19 @@ class GUI(App):
         if 'runnable' in kwargs:
             self.runnable = kwargs['runnable']
             del kwargs['runnable']
+        else:
+            raise ValueError("Missing ``runnable`` from argument list.")
+        # FIXME: side effect, sets windowed/fullscreen mode only on next run
+        if 'size' in kwargs:
+            Config.set('graphics', 'fullscreen', '0')
+            Config.write()
+            self.size = kwargs['size']
+            del kwargs['size']
+        else:
+            self.size = Window.size
+        Window.size = self.size
         super(GUI, self).__init__(**kwargs)
-    
+
     def build(self):
         engine = MainEngine()
         Clock.schedule_interval(engine.update, 1.0 / 25.0)
