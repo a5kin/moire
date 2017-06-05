6c26c75e326245c0a846e00d941ade5635eb7fe2
Andrey <a5kin@users.noreply.github.com>
2017-06-06 01:20:35 +0300

    Panel border is redesigned.

--- moire/widgets/panel.py
+++ moire/widgets/panel.py
@@ -8,17 +8,22 @@ class PanelWidget(FloatLayout):
         super(PanelWidget, self).__init__(**kwargs)
         self.bind(pos=self.redraw)
         self.bind(size=self.redraw)
+        # hardcoded stuff
         self.color = (.1, .6, .6)
-        self.border_width = 1.5
-        self.corner_len = 15
+        self.border_width = 1
+        self.corner_width = 1.5
+        self.corner_len = 7
         self.opacity = .85
 
     def redraw(self, *args):
+        """ Redraw panel on the canvas. """
         self.canvas.clear()
         with self.canvas:
+            # background
             Color(self.color[0] / 2, self.color[1] / 2,
                   self.color[2] / 2, self.opacity)
             Rectangle(pos=self.pos, size=self.size)
+            # border
             Color(self.color[0], self.color[1],
                   self.color[2], 1)
             x, y = self.pos
@@ -27,20 +32,21 @@ class PanelWidget(FloatLayout):
                          y + h, x, y + h, x, y],
                  width=self.border_width,
                  cap='square', joint='miter')
+            # corners
             Color(1, 1, 1, .7)
             Line(points=[x + self.corner_len, y, x, y,
                          x, y + self.corner_len],
-                 width=self.border_width,
+                 width=self.corner_width,
                  cap='square', joint='miter')
             Line(points=[x + w - self.corner_len, y, x + w, y,
                          x + w, y + self.corner_len],
-                 width=self.border_width,
+                 width=self.corner_width,
                  cap='square', joint='miter')
             Line(points=[x, y + h - self.corner_len, x, y + h,
                          x + self.corner_len, y + h],
-                 width=self.border_width,
+                 width=self.corner_width,
                  cap='square', joint='miter')
             Line(points=[x + w, y + h - self.corner_len, x + w, y + h,
                          x + w - self.corner_len, y + h],
-                 width=self.border_width,
+                 width=self.corner_width,
                  cap='square', joint='miter')
