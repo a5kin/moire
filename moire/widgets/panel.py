8978ad7994da616357313743ca9be6fa5833443b
Andrey <a5kin@users.noreply.github.com>
2017-06-05 18:16:04 +0300

    Border for panel widget is implemented.

--- moire/widgets/panel.py
+++ moire/widgets/panel.py
@@ -1,4 +1,4 @@
-from kivy.graphics import Rectangle, Color
+from kivy.graphics import Rectangle, Color, Line
 from kivy.uix.floatlayout import FloatLayout
 
 
@@ -19,3 +19,28 @@ class PanelWidget(FloatLayout):
             Color(self.color[0] / 2, self.color[1] / 2,
                   self.color[2] / 2, self.opacity)
             Rectangle(pos=self.pos, size=self.size)
+            Color(self.color[0], self.color[1],
+                  self.color[2], 1)
+            x, y = self.pos
+            w, h = self.size
+            Line(points=[x, y, x + w, y, x + w,
+                         y + h, x, y + h, x, y],
+                 width=self.border_width,
+                 cap='square', joint='miter')
+            Color(1, 1, 1, .7)
+            Line(points=[x + self.corner_len, y, x, y,
+                         x, y + self.corner_len],
+                 width=self.border_width,
+                 cap='square', joint='miter')
+            Line(points=[x + w - self.corner_len, y, x + w, y,
+                         x + w, y + self.corner_len],
+                 width=self.border_width,
+                 cap='square', joint='miter')
+            Line(points=[x, y + h - self.corner_len, x, y + h,
+                         x + self.corner_len, y + h],
+                 width=self.border_width,
+                 cap='square', joint='miter')
+            Line(points=[x + w, y + h - self.corner_len, x + w, y + h,
+                         x + w - self.corner_len, y + h],
+                 width=self.border_width,
+                 cap='square', joint='miter')
