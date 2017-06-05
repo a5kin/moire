46f07754e0ebd6a04075b486f4fa23b38c84d6b6
Andrey <a5kin@users.noreply.github.com>
2017-06-05 18:15:22 +0300

    Background for panel widget is implemented.

--- moire/widgets/panel.py
+++ moire/widgets/panel.py
@@ -1,3 +1,4 @@
+from kivy.graphics import Rectangle, Color
 from kivy.uix.floatlayout import FloatLayout
 
 
@@ -7,6 +8,14 @@ class PanelWidget(FloatLayout):
         super(PanelWidget, self).__init__(**kwargs)
         self.bind(pos=self.redraw)
         self.bind(size=self.redraw)
+        self.color = (.1, .6, .6)
+        self.border_width = 1.5
+        self.corner_len = 15
+        self.opacity = .85
 
     def redraw(self, *args):
         self.canvas.clear()
+        with self.canvas:
+            Color(self.color[0] / 2, self.color[1] / 2,
+                  self.color[2] / 2, self.opacity)
+            Rectangle(pos=self.pos, size=self.size)
