88e72d488adb36814710511a4d32e77479aeee25
Andrey <a5kin@users.noreply.github.com>
2018-03-04 00:34:34 +0200

    Refactor tests (#7 #9)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 """Tests for base Moire stuff."""
 import unittest
-from functools import partial
 
 from kivy.clock import Clock
 
@@ -11,65 +10,57 @@ from examples.noisetv import NoiseTV
 class TestMainApp(unittest.TestCase):
     """Tests for main app."""
 
-    def pause(*args):
-        """Sleep for some time."""
-
-    def check_run(self, app, *args):
+    def check_run(self, *_):
         """Check app is running."""
-        self.assertGreater(app.runnable.timestep, 0,
+        self.assertGreater(self.app.runnable.timestep, 0,
                            "Environment did not run.")
 
     def press_key(self, key):
         """Emulate key pressing."""
         self.app.engine._on_keyboard_down(None, (None, key), None, None)
 
-    def stop_app(self, app, *args):
+    def stop_app(self, *_):
         """Stop main application."""
         self.press_key("escape")
 
-    def increase_speed(self, app, *args):
+    def increase_speed(self, *_):
         """Speed emulation up."""
-        app.runnable.apply_speed(23)
+        self.app.runnable.apply_speed(23)
 
-    def decrease_speed(self, app, *args):
+    def decrease_speed(self, *_):
         """Speed emulation dowd."""
-        self.assertGreater(app.runnable.speed, 1,
+        self.assertGreater(self.app.runnable.speed, 1,
                            "Speed has not increased.")
-        app.runnable.apply_speed(-23)
-        self.assertEqual(app.runnable.speed, 1,
+        self.app.runnable.apply_speed(-23)
+        self.assertEqual(self.app.runnable.speed, 1,
                          "Speed has not decreased.")
 
-    def toggle_sysinfo(self, app, *args):
+    def toggle_sysinfo(self, *_):
         """Show/hide system info panel."""
-        app.engine.sysinfo.toggle()
-
-    def schedule(self, action, time_sec):
-        """Schedule ``action`` function at ``time``."""
-        p = partial(action, self.app)
-        Clock.schedule_once(p, time_sec)
+        self.app.engine.sysinfo.toggle()
 
     def test_windowed(self):
         """Run the windowed environment and schedule test actions."""
         environment = NoiseTV()
         self.app = moire.GUI(runnable=environment, size=(640, 480))
-        self.schedule(self.check_run, 1)
-        self.schedule(self.increase_speed, 1.2)
-        self.schedule(self.decrease_speed, 2.5)
-        self.schedule(self.toggle_sysinfo, 1)
-        self.schedule(self.toggle_sysinfo, 2)
-        self.schedule(self.stop_app, 3)
+        Clock.schedule_once(self.check_run, 1)
+        Clock.schedule_once(self.increase_speed, 1.2)
+        Clock.schedule_once(self.decrease_speed, 2.5)
+        Clock.schedule_once(self.toggle_sysinfo, 1)
+        Clock.schedule_once(self.toggle_sysinfo, 2)
+        Clock.schedule_once(self.stop_app, 3)
         self.app.run()
 
     def test_fullscreen(self):
         """Run the environment fullscreen."""
         environment = NoiseTV()
         self.app = moire.GUI(runnable=environment)
-        self.schedule(self.stop_app, 1)
+        Clock.schedule_once(self.stop_app, 1)
         self.app.run()
 
     def test_illegal(self):
         """Run the environment without runnable."""
         with self.assertRaises(ValueError):
             self.app = moire.GUI()
-            self.schedule(self.stop_app, 1)
+            Clock.schedule_once(self.stop_app, 1)
             self.app.run()
