8a6f17893015b33e10a542cd36ed4ccae4ecc4c9
Andrey <a5kin@users.noreply.github.com>
2018-03-14 22:47:09 +0200

    Add description of moire package (#7, #10)

--- moire/__init__.py
+++ moire/__init__.py
@@ -1,3 +1,24 @@
+"""
+The official GUI for `Artipixoids!`_ project.
+
+It is integrating natively with `Xentica`_ framework, but you may also
+use it to visualize any step-by-step simulations.
+
+The engine is dealing nothing with rendering, it's relying fully on
+frames rendered with your simulation. So, you should manually
+implement the stuff like zooming or scrolling, and bind the actions to
+keyboard/mouse/etc events.
+
+The only thing Moire doing right now, is keeping the rate of your
+rendered frames, speeding simulation up/down as necessary. Also,
+system info dialog is included, showing current timestep, frames/steps
+per second and simulation speed.
+
+.. _Artipixoids!: http://artipixoids.a5kin.net/concept/artipixoids_concept.pdf
+.. _Xentica: https://github.com/a5kin/xentica/
+
+"""
+
 from moire.main import GUI
 
 
