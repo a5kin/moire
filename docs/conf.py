7e40c6e84f015ffdee97b4d3b91c025d6929cd4e
Andrey <a5kin@users.noreply.github.com>
2018-03-28 00:10:27 +0300

    Mock up kivy for Readthedocs (#7, #12)

--- docs/conf.py
+++ docs/conf.py
@@ -90,9 +90,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'nature'
 html_theme = 'sphinx_rtd_theme'
-#html_theme = 'bootstrap'
 # html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
@@ -177,14 +175,22 @@ texinfo_documents = [
 
 
 class Mock(MagicMock):
+    """Mock uninstallable classes."""
+
     @classmethod
     def __getattr__(cls, name):
+        """Get common attribute."""
         if name == "cached_property":
             return property
         return MagicMock()
 
 
 MOCK_MODULES = [
-    'kivy.config',
+    'kivy.app', 'kivy.uix.floatlayout', 'kivy.clock',
+    'kivy.core.window', 'kivy.properties',
+    'kivy.graphics.texture', 'kivy.graphics',
+    'kivy.resources', 'kivy.logger', 'kivy.config',
+    'kivy.graphics', 'kivy.animation',
+    'kivy.uix.gridlayout', 'kivy.uix.label',
 ]
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
