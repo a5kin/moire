862c7e65b9b705fb3f4139b036eda0d045848f02
Andrey <a5kin@users.noreply.github.com>
2018-04-10 23:27:51 +0300

    Add docstrings for widgets.sysinfo module (#7, #10)

--- moire/widgets/sysinfo.py
+++ moire/widgets/sysinfo.py
@@ -1,3 +1,4 @@
+"""Module for widget with system info."""
 from kivy.core.window import Window
 from kivy.properties import NumericProperty
 
@@ -6,10 +7,27 @@ from moire.widgets.dlist import DescriptiveList
 
 
 class SystemInfoWidget(PanelWidget):
+    """
+    Build-in widget showing system info.
 
+    The fields are:
+
+       FPS
+          Current frames per second speed (for GUI).
+       SPS
+          Current steps per second speed (for simulation).
+       Speed
+          How many steps are skipped each frame.
+       Time
+          Global timestep counter.
+
+    """
+
+    #: Desired frame rate
     fps = NumericProperty()
 
     def __init__(self, **kwargs):
+        """Initialize widget."""
         global_w, global_h = Window.size
         width, height = int(global_w * 0.115), int(global_h * 0.08)
         kwargs['pos'] = (global_w - width - 15, global_h - height - 15)
@@ -27,4 +45,5 @@ class SystemInfoWidget(PanelWidget):
         self.add_widget(self.values)
 
     def __setitem__(self, key, value):
+        """Set value by key at runtime."""
         self.values[key] = value
