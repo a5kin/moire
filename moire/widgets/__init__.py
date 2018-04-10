df582164946929392958a02f89d69b32785148bb
Andrey <a5kin@users.noreply.github.com>
2018-04-10 23:26:34 +0300

    Add description for moire.widgets package (#7, #10)

--- moire/widgets/__init__.py
+++ moire/widgets/__init__.py
@@ -1,3 +1,17 @@
+"""
+The package with the widgets and UI components.
+
+You may use ``widgets`` package as a shortcut to the following
+classes.
+
+- ``widgets.PanelWidget`` →
+  :class:`moire.widgets.panel.PanelWidget`
+- ``widgets.SystemInfoWidget`` →
+  :class:`moire.widgets.sysinfo.SystemInfoWidget`
+- ``widgets.DescriptiveList`` →
+  :class:`moire.widgets.dlist.DescriptiveList`
+
+"""
 from moire.widgets.panel import PanelWidget
 from moire.widgets.sysinfo import SystemInfoWidget
 from moire.widgets.dlist import DescriptiveList
